79d4ba8a4ce2048a99c39354f8933ee38f932db1
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-02-05 10:06:34 -0600

    Add yq binaries for yaml parsing

---
+++ scripts/bin/yq/imports.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## @file
+# Import management for vendored binaries
+#
+# Copyright (c) 2023, Cory Bennett. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+##
+
+
+case "$OSTYPE" in
+  darwin*) yq_bin='yq_darwin_amd64' ;; 
+  linux*)  yq_bin='yq_linux_amd64' ;;
+  msys*)   yq_bin='yq_windows_amd64' ;;
+esac
+yq=$(realpath ./bin/yq/$yq_bin)
+# YQ_RELEASE="https://github.com/mikefarah/yq/releases/download/v4.30.8"
+# mkdir -p ${yq%/*} && curl -o $yq -sL "$YQ_RELEASE/${yq##*/}" > /dev/null 2>&1
+# chmod +x $yq
