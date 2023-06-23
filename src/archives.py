f87d9ad87edcaf4d5d8a8ad9723c9383ef2780d7
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-06-23 05:14:32 -0500

    archives: Add opencore archive method

--- src/archives.py
+++ src/archives.py
@@ -9,13 +9,15 @@ from cgi import parse_header
 from contextlib import contextmanager
 from pathlib import Path
 from ssl import _create_unverified_context as skip_ssl_verify
-from shutil import rmtree, unpack_archive
+from shutil import copytree, rmtree, unpack_archive
 from tempfile import mkdtemp, NamedTemporaryFile
 from urllib.request import urlopen, Request
 
-from typing import Generator
+from typing import Generator, Literal
 
 from parsers.plist import parse_plist
+from constants import OPENCORE_BINARY_DATA_URL
+from filesystem import move, glob
 
 
 @contextmanager
@@ -55,7 +57,7 @@ def extract_archive(url: str | Request,
     if not persist: rmtree(tmp_dir)
 
 @contextmanager
-def extract_kexts(url: str,
+def extract_kext_archive(url: str,
                   persist: bool=False) -> Generator[dict, any, None]:
   """Extracts Kexts from a URL and yields a temporary extraction dictionary.
 
@@ -118,3 +120,45 @@ def extract_kexts(url: str,
   finally:
     # Cleanup after context exits
     if not persist: rmtree(pkg)
+
+@contextmanager
+def extract_opencore_archive(url: str,
+                             target: Literal['IA32', 'X64']='X64',
+                             persist: bool=False) -> Generator[Path, any, None]:
+  """"""
+  tmp_dir = mkdtemp()
+  try:
+    # Extract specified OpenCore package
+    with extract_archive(url) as pkg:
+      # Extract EFI binaries and tree structure
+      EFI_DIR = move(glob(pkg, pattern=f'**/{target}/EFI', first=True), tmp_dir)
+      # Extract ACPI samples
+      acpi_samples = glob(pkg, pattern='**/Docs/AcpiSamples/Binaries/*.aml')
+      for file in acpi_samples: move(file, EFI_DIR.joinpath('OC', 'ACPI'))
+      # Extract sample config.plist
+      sample_plist = glob(pkg, pattern='**/Docs/Sample.plist', first=True)
+      move(sample_plist, EFI_DIR.joinpath('OC'), name='config.plist')
+      
+      # Extract documentation files
+      DOCS_DIR = EFI_DIR.joinpath('..', 'Docs')
+      changelog_doc = glob(pkg, pattern='**/Docs/Changelog.md', first=True)
+      move(changelog_doc, DOCS_DIR)
+      config_doc = glob(pkg, pattern='**/Docs/Configuration.pdf', first=True)
+      move(config_doc, DOCS_DIR)
+
+      # Extract bundled utilities
+      UTILITIES_DIR = EFI_DIR.joinpath('..', 'Utilities')
+      for dir in glob(pkg, pattern='**/Utilities/*/'): move(dir, UTILITIES_DIR)
+
+    # Clone latest additional OpenCore binaries not shipped in the main package
+    with extract_archive(OPENCORE_BINARY_DATA_URL) as pkg:
+      for dir in glob(pkg, pattern='*/'):
+        # Clone additional binaries to EFI directory
+        if EFI_DIR.joinpath('OC', dir.name).exists():
+          copytree(dir, EFI_DIR.joinpath('OC'), dirs_exist_ok=True)
+    
+    # Yield the temporary directory.
+    yield Path(tmp_dir)
+  finally:
+    # Cleanup after context exits
+    if not persist: rmtree(tmp_dir)
