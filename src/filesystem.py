6d1df8a234af5a2136cddf80e7ada4f31a7c6e6b
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-06-23 02:26:31 -0500

    Extend filesystem methods, move archive methods to separate `archive` file

--- src/filesystem.py
+++ src/filesystem.py
@@ -1,118 +1,50 @@
 ## @file
-# Methods for handling and extracting remote archives.
+# Methods for handling cross-platform file system operations.
 #
 # Copyright (c) 2023, Cory Bennett. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 ##
 
-from cgi import parse_header
-from contextlib import contextmanager
+from os import PathLike
 from pathlib import Path
-from ssl import _create_unverified_context as skip_ssl_verify
-from shutil import rmtree, unpack_archive
-from tempfile import mkdtemp, NamedTemporaryFile
-from urllib.request import urlopen, Request
-
-from typing import Generator
-
-from parsers.plist import parse_plist
+from shutil import move as shutil_move
 
 
-@contextmanager
-def extract_archive(url: str | Request,
-                    persist: bool=False) -> Generator[Path, str, None]:
-  """Extracts a file from a URL and yields a temporary extraction directory.
+def move(src: str | PathLike[str],
+         target: str | PathLike[str]):
+  """Moves a file or directory to a new location.
+
+  This is a simple wrapper over shutil's `move` method that
+  recursively creates missing directories in the target path.
 
   Args:
-    url: URL of the archive file.
-    persist: Flag to disable cleanup of the temporary directory.
+    src: Source path.
+    target: Destination path.
 
-  Yields:
-    tmp_dir (str): Path to the temporary directory.
-
-  Example:
-    >>> with extract_archive('https://example.com/foo.zip') as tmp_dir:
-    print(tmp_dir)
-    # -> "/tmp/xxxxxx"
+  Returns:
+    The destination path.
   """
-  tmp_dir = mkdtemp()
-  try:
-    with urlopen(url, context=skip_ssl_verify()) as response:
-      # Extract filename from request headers.
-      _, params = parse_header(response.headers.get('Content-Disposition', ''))
-      filename = params['filename']
-      extension = "".join(Path(filename).suffixes)
-      # Write archive to a temporary file.
-      with NamedTemporaryFile(suffix=extension) as tmp_file:
-        tmp_file.write(response.read())
-        tmp_file.seek(0)
-        # Extract the zip file to the temporary directory.
-        unpack_archive(tmp_file.name, tmp_dir)
-    # Yield the temporary directory.
-    yield Path(tmp_dir)
-  finally:
-    # Cleanup after context exits
-    if not persist: rmtree(tmp_dir)
+  dest = Path(target, Path(src).name)
+  if not (parent_dir := dest.parent).is_dir() and str(parent_dir) != '.':
+    parent_dir.mkdir(parents=True, exist_ok=True)
+  return shutil_move(src, parent_dir)
 
-@contextmanager
-def extract_kexts(url: str,
-                  persist: bool=False) -> Generator[dict, any, None]:
-  """Extracts Kexts from a URL and yields a temporary extraction dictionary.
+def glob(directory: str | PathLike[str],
+         pattern: str,
+         first: bool = False) -> list[Path] | Path | None:
+  """Returns a list of paths matching the given pattern.
 
   Args:
-    url: URL of the archive file.
-    persist: Flag to disable cleanup of the temporary directory.
+    directory: Directory to search.
+    pattern: Glob pattern.
+    first: Whether to return only the first match.
 
-  Yields:
-    kexts (dict): Dictionary of extracted kexts.
-
-  Example:
-    >>> with extract_kexts('https://example.com/foo.zip') as kexts:
-    ...   print(kexts)
-    # -> {
-    #   'Foo': {
-    #     '__path': Path('/tmp/foo/Foo.kext'),
-    #     '__url': 'https://example.com/foo.zip',
-    #     'identifier': 'com.example.foo',
-    #     'version': '1.0.0',
-    #     'extract': './Foo.kext',
-    #     'dependencies': {
-    #       'com.example.bar': '1.0.0'
-    #     }
-    #   }
-    # }
+  Returns:
+    A list of matching paths.
+    Instead returns the first matching path if `first` is `True`.
+    Returns `None` if no matches are found.
   """
-  kexts = dict()
-  try:
-    with extract_archive(url, persist=True) as pkg:
-      for plist_path in pkg.glob('**/*.kext/Contents/Info.plist'):
-        kext_path = Path(plist_path).parents[1]
-        extract_path = f'.{str(kext_path)[len(str(pkg)):]}'
-        with open(plist_path, 'r', encoding='UTF-8') as file:
-          # Build plist dictionary from filestream
-          plist = parse_plist(file)
-          # Extract Kext bundle properties
-          name = plist['CFBundleName'][1]
-          identifier = plist['CFBundleIdentifier'][1]
-          version = plist['CFBundleVersion'][1]
-          libraries = { k:v[1] for k,v in plist['OSBundleLibraries'].items()
-                          # Ignore self-dependencies
-                      if (not k == identifier and
-                          # Ignore Apple-provided libraries
-                          not k.startswith('com.apple.')) }
-          # Cleanup
-          del plist
-        # Update kext dictionary
-        kexts[name] = {
-          "__path": kext_path,
-          "__url": url,
-          "identifier": identifier,
-          "version": version,
-          "extract": extract_path,
-          "dependencies": libraries
-        }
-    # Yield the kexts dictionary.
-    yield kexts
-  finally:
-    # Cleanup after context exits
-    if not persist: rmtree(pkg)
+  matches = list(Path(directory).glob(pattern))
+  if len(matches):
+    return matches if not first else matches[0]
+  return None
