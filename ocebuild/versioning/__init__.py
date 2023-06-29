6a4dd35647fb0430956f8db67ac74f0fd95e1ba6
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-06-29 04:06:26 -0500

    Fix setuptools module detection

--- ocebuild/versioning/__init__.py
+++ ocebuild/versioning/__init__.py
@@ -1,6 +1,6 @@
 ## @file
 # Copyright (c) 2023, Cory Bennett. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: BSD-3-Clause
 ##
 
 """This empty file is needed to be recognized as a package by the setuptools."""
