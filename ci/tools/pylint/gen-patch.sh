38beca48970405dd0374fe26c8118c6314f97916
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-07-18 18:31:20 -0500

    Add patch to reflect changes to Google pylint config

---
+++ ci/tools/pylint/gen-patch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC1091,SC2164
+
+## @file
+# Generates a patch file for pylintrc from the Google pylint config.
+#
+# Copyright (c) 2023, Cory Bennett. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+##
+
+# Change CWD for imports
+__PWD__=$(pwd); cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Generate patch
+git diff --patch --no-index pylintrc-google pylintrc > pylintrc.patch
