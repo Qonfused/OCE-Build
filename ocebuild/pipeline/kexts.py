d355e12d50b6436291256dc11bad331516535eea
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-08-08 15:30:16 -0500

    [pipeline/kexts] Refactor node sorting for better readability

--- ocebuild/pipeline/kexts.py
+++ ocebuild/pipeline/kexts.py
@@ -152,8 +152,7 @@ def sort_kext_cfbundle(filepaths: List[Union[str, Path]]) -> OrderedDict:
               has_mutual_dependency = dependency in next_entry['dependencies']
               if matches and (has_mutual_keys or has_mutual_dependency):
                 insertion_index += 1
-              else:
-                break
+              else: break
           except IndexError:
             pass #de-op
         # Upsert standalone dependents with dependencies/mutual dependents
@@ -169,27 +168,28 @@ def sort_kext_cfbundle(filepaths: List[Union[str, Path]]) -> OrderedDict:
   def num_dependents(kext: dict) -> int:
     return sum(1 if kext['identifier'] in k['dependencies'] else 0
                for k in sorted_dependencies)
-  def has_resolved_dependencies(kext: dict) -> bool:
-    return any(k in identifier_map for k in kext.get('dependencies', {}))
-  def is_bundled_kext(kext: dict) -> bool:
-    return kext['__path'].count('.kext') > 1
-  def is_standalone_kext(kext: dict) -> bool:
-    return not num_dependents(kext) and not is_bundled_kext(kext)
 
   # Group each node alphabetically by dependency name
   offset = 0
   nodes = []
   for idx, kext in enumerate(sorted_dependencies):
-    if is_standalone_kext(kext) and not has_resolved_dependencies(kext):
+    has_resolved_dependencies = any(k in identifier_map
+                                    for k in kext.get('dependencies', {}))
+    is_bundled_kext = kext['__path'].count('.kext') > 1
+    is_standalone_kext = not num_dependents(kext) and not is_bundled_kext
+    # Has no dependencies on any nodes' dependents
+    if is_standalone_kext and not has_resolved_dependencies:
       nodes.append([kext])
       offset = idx + 1
-    elif num_dependents(kext) and not (is_bundled_kext(kext), offset == idx):
+    # Has dependents in the current node
+    elif num_dependents(kext) and not (is_bundled_kext, offset == idx):
       offset = idx + 1
       nodes.append(sorted_dependencies[offset:idx])
-    elif not (has_resolved_dependencies(kext) or is_bundled_kext(kext)):
+    # Is bundled or has dependencies in the current node
+    elif not (has_resolved_dependencies or is_bundled_kext):
       nodes.append(sorted_dependencies[offset:idx])
       offset = idx
-    # Last node
+    # Ensures the last node is added
     elif idx == len(sorted_dependencies) - 1:
       nodes.append(sorted_dependencies[offset:idx])
     else: continue
