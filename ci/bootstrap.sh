d2206de273c841345133578b412c4c35ba202070
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-12-05 08:23:57 -0600

    Create CLI bootstrap script for consuming projects

---
+++ ci/bootstrap.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+## @file
+# Downloads and executes a binary release of the OCE Build CLI.
+#
+# Copyright (c) 2023, The OCE Build Authors. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+##
+
+OCEBUILD_URL="https://github.com/Qonfused/OCE-Build"
+OCEBUILD_VERSION="nightly"
+
+#region Shell Macros
+
+os() {
+  if   [[ "$OSTYPE" == "msys" || $(grep -i Microsoft /proc/version) ]]; then
+    echo "windows"
+  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "linux"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "macos"
+  fi
+}
+
+ext() {
+  case $(os) in
+    windows*) echo "$1.exe";;
+    linux*)   echo "$1.linux";;
+    macos*)   echo "$1";;
+  esac
+}
+
+cmd() {
+  [[ $(os) == "windows" ]] && echo "$1.exe" || echo "$1"
+}
+
+#endregion
+
+# Arrange for the binary to be deleted when the script terminates
+BINARY_PATH="./$(ext ocebuild)"
+trap 'rm -f "$BINARY_PATH"' 0
+trap 'exit $?' 1 2 3 15
+
+# Download the binary release for the current platform
+BINARY_URL="$OCEBUILD_URL/releases/download/$OCEBUILD_VERSION/$(ext ocebuild)"
+$(cmd curl) -sSL -k $BINARY_URL > "$BINARY_PATH"
+
+# Executes OCE Build with provided arguments
+if [[ $(os) != "windows" ]]; then chmod +x "$BINARY_PATH" fi
+$BINARY_PATH "$@"
