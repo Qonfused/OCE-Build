9d7d8ca21c0162087718e93b93689f0753d19c49
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-02-08 16:48:17 -0600

    Fix bootstrap.sh newline

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -50,7 +50,7 @@ echo "Downloading OCE Build CLI..."
 # Download the binary release for the current platform
 BINARY_URL="$OCEBUILD_URL/releases/download/$OCEBUILD_VERSION/$(ext ocebuild)"
 $(cmd curl) -sSL -k $BINARY_URL > "$BINARY_PATH"
-echo "Done.\n"
+echo -e "Done.\\n"
 
 # Executes OCE Build with provided arguments
 if [[ $(os) != "windows" ]]; then chmod +x "$BINARY_PATH"; fi
