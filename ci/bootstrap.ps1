81c313747806af7cabe8cd536243f8ae925593b4
Cory Bennett <csquaredbennett@gmail.com>
2024-09-03 08:43:18 -0500

    Update bootstrap.ps1 for older Powershell versions
    Sets OS variables for properly detecting the current platform.

--- ci/bootstrap.ps1
+++ ci/bootstrap.ps1
@@ -10,18 +10,30 @@ param (
   [parameter(ValueFromRemainingArguments)][string[]]$arguments
 )
 
-$OCEBUILD_URL="https://github.com/Qonfused/OCE-Build"
-$OCEBUILD_VERSION="nightly"
+# Trap native errors halt script execution on any error
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
+# Check if the powershell version is less than 6.0
+if ($PSVersionTable.PSVersion.Major -lt 6) {
+  # Patch OS detection to properly identify the current platform
+  # on older versions of PowerShell (non-Core distributions).
+  if (-not $IsWindows) {
+    $IsWindows = [System.Environment]::OSVersion.Platform -lt [System.PlatformID]::Unix
+  }
+  if (-not $IsLinux) {
+    $IsLinux = ([System.Environment]::OSVersion.Platform -ge [System.PlatformID]::Unix) -and ([System.Environment]::OSVersion.Platform -ne [System.PlatformID]::MacOSX)
+  }
+  if (-not $IsMacOS) {
+    $IsMacOS = [System.Environment]::OSVersion.Platform -eq [System.PlatformID]::MacOSX
+  }
+}
 
 function Bootstrap {
   param (
     [string]$uri,
     [string]$dest
   )
-
-  # Trap native errors halt script execution on any error
-  $PSNativeCommandUseErrorActionPreference = $true
-  $ErrorActionPreference = 'Stop'
 
   # Download the executable from the specified URI
   Invoke-WebRequest -Uri $uri -OutFile $dest
@@ -33,6 +45,9 @@ function Bootstrap {
 }
 
 Write-Host "Downloading OCE Build CLI..."
+
+$OCEBUILD_URL="https://github.com/Qonfused/OCE-Build"
+$OCEBUILD_VERSION="nightly"
 
 # Check the OS platform to determine the executable file extension
 $EXT = if ($IsWindows) { ".exe" } elseif ($IsLinux) { ".linux" } else { "" }
